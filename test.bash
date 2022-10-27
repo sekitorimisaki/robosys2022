98e6cdc0e0472fde26a2d2a6e1c096f554de9cd3
sekitorimisaki <sekitorimisaki@DESKTOP-THJNPKR.localdomain>
2022-10-27 12:51:29 +0900

    テスト項目の追加と山田の削除

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 ng(){
@@ -8,7 +8,12 @@ ng(){
 res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
